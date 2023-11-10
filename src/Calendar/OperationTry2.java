4392a2c087d45a706267b423cf4c340dad8fd6fd
Zichao Cai <zichao.cai94@gmail.com>
2023-11-10 15:35:10 +0100

    operate with postgreSQL

---
+++ src/Calendar/OperationTry2.java
@@ -0,0 +1,183 @@
+package Calendar;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class OperationTry2 {
+	public static void main(String args[]) {
+		
+		String user = "Lina Reich";
+		
+		int vacationPerYear = 30;
+		int[] vacationIDs = new int[vacationPerYear];
+
+		String chosenDateStart = "2023-12-24";
+		String chosenDateEnd = "2023-12-25";
+		
+		Date dateStart =Date.valueOf(chosenDateStart);
+		Date dateEnd =Date.valueOf(chosenDateEnd);
+		
+		int chosenYear = Integer.parseInt(chosenDateStart.substring(0, 4));
+		//boolean isTheSameYear = chosenYear == Integer.parseInt(chosenDateEnd.substring(0, 4));
+		
+		boolean insertTimePossible = LocalDate.now().isBefore(LocalDate.parse(chosenDateStart));
+		System.out.println(insertTimePossible);
+		
+		Connection c = null;
+		
+		String JDBC_URL = "jdbc:postgresql://localhost:5432/vacation";
+		String JDBC_USER = "postgres";
+		String JDBC_PASSWORD = "123";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+			c.setAutoCommit(false);
+			//System.out.println("Opened database successfully");
+			
+
+			int tmpID = VacationMethod.insertVacation(c, user, chosenYear, dateStart, dateEnd);
+			
+			//java IO
+			if (tmpID != 0) {
+				for (int i = 0; i < vacationIDs.length; i++) {
+					if (vacationIDs[i] == 0) {
+						vacationIDs[i] = tmpID;
+						break;}
+				}	
+			}
+			//VacationMethod.updateVacation(c, user, 2, dateStart, dateEnd);
+			//VacationMethod.deleteVacation(c, user, 2);
+			//VacationMethod.showDaysAmount(c, user, chosenYear);
+
+			c.commit();
+			c.close();
+			
+			System.out.println("Operate database successfully");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+ ": "+e.getMessage());
+			System.exit(0);
+		}
+		
+		System.out.println(Arrays.toString(vacationIDs));
+		
+	}
+	
+	
+	class VacationMethod{
+		
+		static String TableName = new String("VACATION_LIST");
+		static String TableColumn1 = new String("NAME");
+		static String TableColumn2 = new String("YEAR");
+		static String TableColumn3 = new String("DATE_START");
+		static String TableColumn4 = new String("DATE_END");
+		static String TableColumn5 = new String("DAYS");
+		
+		public static int insertVacation(Connection c, String user, int chosenYear, Date chosenDateStart, Date chosenDateEnd) {
+			String sql = String.format("INSERT INTO %s (%s, %s, %s, %s) "
+					+ "SELECT INITCAP(?), ?, ?, ? WHERE "
+					+ "NOT EXISTS (SELECT * FROM %s WHERE %s = ? AND %s = ? AND %s = ?);",
+					TableName, TableColumn1, TableColumn2, TableColumn3, TableColumn4,
+					TableName, TableColumn1, TableColumn3, TableColumn4);
+			try (PreparedStatement ps = c.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)){
+				ps.setObject(1, user);
+				ps.setObject(2, chosenYear);
+				ps.setDate(3, chosenDateStart);
+				ps.setDate(4, chosenDateEnd);
+				ps.setObject(5, user);
+				ps.setDate(6, chosenDateStart);
+				ps.setDate(7, chosenDateEnd);
+				int i = ps.executeUpdate();
+				try (ResultSet rs = ps.getGeneratedKeys()){
+					if (rs.next()) {
+						int id = rs.getInt(1);
+						return id;
+					}
+				}
+				
+				System.out.println("If data is written: "+ i);
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return 0;
+		}
+		
+		public static void updateVacation(Connection c, String user, int id, Date chosenDateStart, Date chosenDateEnd) {
+			String sql = String.format("UPDATE %s SET %s = ?, %s = ? WHERE id = ?;",
+					TableName, TableColumn3, TableColumn4);
+			try (PreparedStatement ps = c.prepareStatement(sql)){
+				ps.setDate(1, chosenDateStart);
+				ps.setDate(2, chosenDateEnd);
+				ps.setObject(3, id);
+				int i = ps.executeUpdate();
+				
+				System.out.println("If data is written: "+ i);
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public static void deleteVacation(Connection c, String user, int id) {
+			String sql = String.format("DELETE FROM %s WHERE id = ?;",
+					TableName);
+			try (PreparedStatement ps = c.prepareStatement(sql)){
+				ps.setObject(1, id);
+				int i = ps.executeUpdate();
+				
+				System.out.println("If data is written: "+ i);
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+		public static void showDaysAmount(Connection c, String user, int chosenYear) {
+			String sql = String.format("SELECT id, %s, %s, %s, %s, %s "
+					+ "FROM %s WHERE %s = ? AND %s = ?;",
+					TableColumn1, TableColumn2, TableColumn3, TableColumn4, TableColumn5,
+					TableName, TableColumn1, TableColumn2);
+			try (PreparedStatement ps = c.prepareStatement(sql)){
+				ps.setObject(1, user);
+				ps.setObject(2, chosenYear);
+				try (ResultSet rs = ps.executeQuery()){
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String name = rs.getString("name");
+						int year = rs.getInt("year");
+						String dateStart = rs.getString("date_start");
+						String dateEnd = rs.getString("date_end");
+						int days = rs.getInt("days");
+						
+						System.out.println("ID = " + id);
+						System.out.println("NAME = " + name);
+						System.out.println("YEAR = " + year);
+						System.out.println("DATE_START = " + dateStart);
+						System.out.println("DATE_END = " + dateEnd);
+						System.out.println("DAYS = " + days);
+						System.out.println();
+					}
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+
+	}
+}
--- src/Calendar/PostgreSQLTest.java
+++ src/Calendar/PostgreSQLTest.java
@@ -7,43 +7,39 @@ import java.sql.ResultSet;
 
 public class PostgreSQLTest {
 
+	protected static int id = 1;
+	
 	public static void main(String args[]) {
+		String user = "Lina";
+		int chosenYear = 2023;
+		String chosenDate = "2023-12-24";
 		Connection c = null;
 		Statement stmt = null;
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager
-					.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "123");
+					.getConnection("jdbc:postgresql://localhost:5432/vacation", "postgres", "123");
 			c.setAutoCommit(false);//set false, then should be committed later
 			System.out.println("Opened database successfully");
 			
 			stmt = c.createStatement();
 			/*create table
-			String sql = "CREATE TABLE CALENDARBASE" +
+			String sql = "CREATE TABLE VACATIONLIST" +
 					"(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " DATE           INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50) )";
+                    " NAME           TEXT    NOT NULL," +
+                    " YEAR           INT     NOT NULL," +
+                    " DATE			 DATE );";
 			stmt.executeUpdate(sql);
 			*/
 			
-			/*Insert data
-			String sql = "INSERT INTO CALENDARBASE (ID,NAME,DATE,ADDRESS)" +
-					"VALUES (1, 'Alice', 20231103, 'Berlin');";
+			//Insert data
+			String sql = String.format("INSERT INTO VACATIONLIST (ID,NAME,YEAR,DATE) " +
+					"VALUES (%d, '%S', %d, '%s');", id, user, chosenYear, chosenDate);
+
 			stmt.executeUpdate(sql);
+			c.commit();
+			id ++;
 			
-			sql = "INSERT INTO CALENDARBASE (ID,NAME,DATE,ADDRESS)" +
-					"VALUES (2, 'Bob', 20231130, 'Leipzig');";
-			stmt.executeUpdate(sql);
-			
-			sql = "INSERT INTO CALENDARBASE (ID,NAME,DATE,ADDRESS)" +
-					"VALUES (3, 'Colin', 20231205, 'London');";
-			stmt.executeUpdate(sql);
-			
-			sql = "INSERT INTO CALENDARBASE (ID,NAME,DATE,ADDRESS)" +
-					"VALUES (4, 'David', 20231228, 'Berlin');";
-			stmt.executeUpdate(sql);
-			*/
 			
 			/*Update data, ***the newest update data will be printed as the last
 			String sql = "UPDATE CALENDARBASE set DATE = 20240101 where ID=1;";
@@ -51,23 +47,23 @@ public class PostgreSQLTest {
 			*/
 			
 			/*Delete data
-	        String sql = "DELETE from CALENDARBASE where ID=2;";
+	        String sql = "DELETE from CALENDARBASE where ID<5;";
 	        stmt.executeUpdate(sql);
 			
 			c.commit();*///for any change of the database (e.g. insert/update/delete data)
 			
 			//Select and read data from database
-			ResultSet rs = stmt.executeQuery("SELECT * FROM CALENDARBASE;");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM VACATIONLIST;");
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
-				int date = rs.getInt("date");
-				String address = rs.getString("address");
+				int year = rs.getInt("year");
+				String date = rs.getString("date");
 				
 				System.out.println("ID = " + id);
 				System.out.println("NAME = " + name);
+				System.out.println("YEAR = " + year);
 				System.out.println("DATE = " + date);
-				System.out.println("ADDRESS = " + address);
 				System.out.println();
 			}
 			rs.close();
